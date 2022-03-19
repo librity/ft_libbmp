e836e7e4eb366b5732a1d97086690840a9250129
Luis Geniole <luis.geniole@gmail.com>
2022-03-19 00:48:59 -0300

    Add mlx pointer to image.

--- sources/mlx_image/destroy.c
+++ sources/mlx_image/destroy.c
@@ -1,36 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   initialize.c                                       :+:      :+:    :+:   */
+/*   destroy.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 23:32:27 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/03/19 00:31:58 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/03/19 00:34:23 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_libbmp.h>
 
-static void	die_if_null(void *ptr)
+void	bm_destroy_mlx_image(t_mlx_image *image)
 {
-	if (ptr == NULL)
-		bm_kill(MLX_IMAGE_INIT);
-}
-
-void	bm_initialize_mlx_image(t_mlx_image *image, void *mlx,
-			int width, int height)
-{
-	image->mlx = mlx;
-	image->img = mlx_new_image(mlx, width, height);
-	die_if_null(image->img);
-	image->data = mlx_get_data_addr(image->img,
-			&image->bits_per_pixel,
-			&image->line_length,
-			&image->endian);
-	die_if_null(image->data);
-	image->width = width;
-	image->height = height;
-	image->half_width = width / 2.0;
-	image->half_height = height / 2.0;
+	mlx_destroy_image(image->mlx, image->img);
 }
