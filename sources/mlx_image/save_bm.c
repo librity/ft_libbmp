6509505d1e85d35b2008ce9af875590d68e1f287
Luis Geniole <luis.geniole@gmail.com>
2022-03-21 15:52:05 -0300

    Fix mlx_image_save_bm bug.

--- sources/mlx_image/save_bm.c
+++ sources/mlx_image/save_bm.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   save_image.c                                       :+:      :+:    :+:   */
+/*   save_bm.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 13:43:41 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/03/21 14:11:21 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/03/21 15:51:19 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ static void	transfer_pixels(t_mlx_image *image, t_bitmap *bm_image)
 		y = image->height;
 		while (y--)
 		{
-			color_int = mlx_image_get_pixel_int(image, y, x);
+			color_int = mlx_image_get_pixel_int(image, x, y);
 			colot_rgb = int_to_rgb(color_int);
 			bm_draw(bm_image, colot_rgb, x, y);
 		}
