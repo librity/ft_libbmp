5ed1d0adea3e7020eaf9072264aff1d72b03c0a2
Luis Geniole <luis.geniole@gmail.com>
2022-03-21 23:20:44 -0300

    Flip y axis on mlx_image_save_bm.

--- sources/mlx_image/save_bm.c
+++ sources/mlx_image/save_bm.c
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 13:43:41 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/03/21 15:51:19 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/03/21 23:14:52 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ static void	transfer_pixels(t_mlx_image *image, t_bitmap *bm_image)
 		{
 			color_int = mlx_image_get_pixel_int(image, x, y);
 			colot_rgb = int_to_rgb(color_int);
-			bm_draw(bm_image, colot_rgb, x, y);
+			bm_draw(bm_image, colot_rgb, x, image->height - 1 - y);
 		}
 	}
 }
